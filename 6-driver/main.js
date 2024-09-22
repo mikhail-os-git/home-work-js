559c58252cb1eb5ebcd185ad5295e98851eb1248
mikhail-os-git <mishaend@gmail.com>
2024-09-22 22:38:20 +0200

    Очистка ветки master

--- 6-driver/main.js
+++
@@ -1,9 +0,0 @@
-const hasLicense = true;
-const are = 18;
-const isDrunk = false;
-
-const canDrive = (are <= 18) && hasLicense && !isDrunk;
-
-console.log(
-	`Имеет право управлять транспортным средством: ${canDrive ? 'Да' : 'Нет'}`
-);
